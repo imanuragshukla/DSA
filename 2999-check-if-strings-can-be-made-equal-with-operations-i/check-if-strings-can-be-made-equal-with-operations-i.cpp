efe48cda5eb768bef6fb2052d329757827f9343f
Anurag Shukla <imanurag2004@gmail.com>
2026-03-29 14:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (49.03%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        for(int i=0;i<2;i++){
+            if(s1[i]!=s2[i] && s1[i]!=s2[i+2])return false;  
+        }
+        for(int i=2;i<4;i++){
+            if(s1[i]!=s2[i] && s1[i]!=s2[i-2])return false;
+        }
+
+        return true;
+    }
+};
