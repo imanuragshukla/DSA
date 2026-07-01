0556c29cb7894344a22bb6b68a0bae2192177ee4
Anurag Shukla <imanurag2004@gmail.com>
2026-07-01 08:03:55 +0530

    Time: 12 ms (26.99%) | Memory: 24.8 MB (10.46%) - LeetSync

---
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lps(string s){
+        vector<int>lps(s.size(),0);
+        int pre =0, suf=1;
+        while(suf<s.size()){
+            if(s[pre]==s[suf]){
+                lps[suf]=pre+1;
+                pre++;suf++;
+            }
+            else{
+                if(pre==0){
+                    lps[suf]=0;
+                    suf++;
+                }
+                else{
+                    pre = lps[pre-1];
+                }
+            }
+        }
+        return lps[s.size()-1];
+    }
+    string longestPrefix(string s) {
+        int a = lps(s);
+        return s.substr(0,a);
+    }
+};
